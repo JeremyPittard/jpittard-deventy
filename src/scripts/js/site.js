d73802fde3ccaaa53482d5b8572c40b94a1a33a3
JeremyPittard <jeremy@jpittard.net>
2019-09-02 23:15:57 +0800

    further tweaking theme

--- src/scripts/js/site.js
+++ src/scripts/js/site.js
@@ -1,9 +1,14 @@
 //init functions
 $(document).ready(function() {
+    loadOut();
     menuOpen();
 });
 
 //functions start here
+
+function loadOut() {
+    console.lot("loaded");
+}
 
 function menuOpen() {
     $(".nav-bar__menu-icon").on("click", function() {
