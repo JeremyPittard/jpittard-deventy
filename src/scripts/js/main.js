ad47e305af26d1339cba7c2aed01e26f1436aba4
JeremyPittard <jeremy@jpittard.net>
2019-09-03 23:58:00 +0800

    built out

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -5,7 +5,5 @@ import "./skip-link-focus-fix";
 //smooth scroll
 import "./smooth-scroll";
 
-// uncomment for animate on scroll  as well as the scss files
-import "./aos/aos";
-
 import "./site";
+import "./waypoints";
